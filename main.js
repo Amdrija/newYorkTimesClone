72ebb197330521d09092b441bd723f3e9007590e
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2020-04-04 18:21:39 +0200

    Add class to link in the article to be able to select only links that need to change text-decoration when they are clicked

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function removeLinkUnderlineOnClick() {
-    let links = document.getElementsByTagName('a');
+    let links = document.getElementsByClassName('article-link');
     for (let link of links) {
         link.addEventListener('click', (event) => (event.target.style.textDecoration = 'none'));
     }
